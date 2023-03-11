7d10eadc4baf0794f1196d04508b687ee24f1566
noarsark <noarsark817@163.com>
2023-03-11 11:34:00 +0800

    default-mod

--- src/default.ts
+++ src/default.ts
@@ -8,6 +8,6 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 - 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 提供支持。
-- 欢迎加我微信noarsark18获取key。
-- 欢迎加入我的[知识星球](https://imagecdn.noarsark.cn/blog/202303102139212.png)，一起玩赚ChatGPT。早加入早下场，目前加入可以优惠30元哦。
+- 欢迎加我[微信noarsark817](https://imagecdn.noarsark.cn/blog/202303111131450.jpg)获取key及使用方法。
+- 欢迎加入我的[知识星球](https://imagecdn.noarsark.cn/blog/202303102139212.png)，获取ChatGPT更多玩法。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
