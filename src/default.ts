8de502c7b0b01797637d5c2f53e6e55e34b0cae6
noarsark <noarsark817@163.com>
2023-03-14 23:59:22 +0800

    加个鸡腿

--- src/default.ts
+++ src/default.ts
@@ -9,5 +9,7 @@ export const defaultSetting = {
 export const defaultMessage = `
 - 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 提供支持。
 - 关注公众号[noarsark](https://imagecdn.noarsark.cn/blog/202302152115832.png)，回复【chatgpt】获取体验key。
+- 欢迎加入[ChatGPT交流群](https://imagecdn.noarsark.cn/blog/202303142355445.jpg)，一起探索ChatGPT最新玩法。
+- 如果网站对你有所帮助，可以给GPT[加个鸡腿](https://imagecdn.noarsark.cn/blog/202303142350287.jpg)。
 - 如需获取ChatGPT独享账号，欢迎加我[微信noarsark18](https://imagecdn.noarsark.cn/blog/chatAI.jpg)购买。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
