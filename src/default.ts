1035cdc54bcbc67985be666ba36d8b0865a4e451
noarsark <noarsark817@163.com>
2023-03-30 22:36:44 +0800

    text

--- src/default.ts
+++ src/default.ts
@@ -7,8 +7,9 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 提供支持。
+- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 提供支持，本网站无需科学上网，任意环境均可使用。
 - 关注公众号[noarsark](https://imagecdn.noarsark.cn/blog/202302152115832.png)，回复【chatgpt】获取体验key。
-- 如需购买独享账号或plus账号可以联系微信[noarsark18](https://imagecdn.noarsark.cn/blog/202303302148247.jpg)。
 - 欢迎加入[ChatGPT交流群](https://imagecdn.noarsark.cn/blog/202303302137596.jpg)，一起探索ChatGPT以及AI绘画最新玩法。
+- 早就是优势，感兴趣可以加入ChatGPT专属付费社群(https://mp.weixin.qq.com/s/ImiI5HXOKRHp7rhQMZUz1g)，带大家系统学习ChatGPT以及AI绘画等前沿技术。
+- 如需购买独享账号或plus账号可以联系微信[noarsark18](https://imagecdn.noarsark.cn/blog/202303302148247.jpg)。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
