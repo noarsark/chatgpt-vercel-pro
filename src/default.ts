c7a32d1d2c8557760968eaac468dcb992342157c
noarsark <noarsark817@163.com>
2023-04-15 11:54:35 +0800

    text

--- src/default.ts
+++ src/default.ts
@@ -7,10 +7,8 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 提供支持，本网站无需科学上网，任意环境均可使用。
-- [ChatGPT Pro会员站](https://chatgpt5.one)，使用更加方便快捷。
-- 关注公众号[noarsark](https://imagecdn.noarsark.cn/blog/202302152115832.png)，回复【chatgpt】获取体验key。
-- [ChatGPT交流圈](https://t.zsxq.com/0c4hMbm3Z)
-- [ChatGPT交流群](https://imagecdn.noarsark.cn/blog/202304020928848.jpg)，一起探索ChatGPT以及AI绘画最新玩法。
+- 本网站无需科学上网，关注公众号[noarsark](https://imagecdn.noarsark.cn/blog/202302152115832.png)，回复【chatgpt】获取体验key。
+- 订阅[ChatGPT Pro](https://chatgpt5.one)，15/月即可获取无限次对话，功能更加强大。
+- 加我好友[noarsark18](https://imagecdn.noarsark.cn/blog/202303302148247.jpg)，免费领取价值99元的ChatGPT以及AI绘画干货资料，并邀请加入ChatGPT交流社群。
 - 如需购买独享账号或plus账号可以联系微信[noarsark18](https://imagecdn.noarsark.cn/blog/202303302148247.jpg)。
-- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
+- 本站使用技巧：<kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 提示词自动显示。点击输入框滚动到底部。`
