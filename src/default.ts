7d92fa8960817d677fb84a6fe00b7b43494cb32c
noarsark <noarsark817@163.com>
2023-05-21 19:52:42 +0800

    msg

--- src/default.ts
+++ src/default.ts
@@ -7,8 +7,7 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 本网站无需科学上网，关注公众号[noarsark](https://imagecdn.noarsark.cn/blog/202302152115832.png)，回复【chatgpt】获取体验key。
-- 订阅[ChatGPT Pro](https://chatgpt5.one)，15/月即可获取无限次对话，功能更加强大。
-- 加我好友[noarsark18](https://imagecdn.noarsark.cn/blog/202303302148247.jpg)，免费领取价值99元的ChatGPT以及AI绘画干货资料，并邀请加入ChatGPT交流社群。
-- 如需购买独享账号或plus账号可以联系微信[noarsark18](https://imagecdn.noarsark.cn/blog/202303302148247.jpg)。
+- 本网站无需科学上网，点击左下角齿轮输入key即可对话。关注公众号[noarsark](https://imagecdn.noarsark.cn/blog/202302152115832.png)，回复【chatgpt】获取体验key。
+- 加入[noarsark和他的朋友们](https://t.zsxq.com/0cnbujGDX)即可获取[ChatGPT Pro](https://chatgpt5.one)一年免费使用以及ChatGPT最新实战案例。
+- 如需进群领取资料或购买plus账号可以添加微信[noarsark18](https://imagecdn.noarsark.cn/blog/202303302148247.jpg)，添加时请备注来意。
 - 本站使用技巧：<kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 提示词自动显示。点击输入框滚动到底部。`
